657b407113aa8f425070a17f694f2334ad3c511a
AdnanHafeez <adnanhr81@gmail.com>
2018-11-06 18:37:54 +1100

    Added the details page and links to the images

--- portfolio-project/portfolio/urls.py
+++ portfolio-project/portfolio/urls.py
@@ -22,4 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',projects.views.adnan,name='home'),
+    path('projects/<int:project_id>',projects.views.details,name='detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio-project/projects/views.py
+++ portfolio-project/projects/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 # Create your views here.
 def adnan(request):
     projects = Project.objects
     return render(request,'projects/home.html', {'projects':projects})
+
+def details(request,project_id):
+    project_detail= get_object_or_404(Project, pk=project_id)
+    return render(request, 'projects/detail.html', {'project':project_detail})
